4efd0eda2ec65ceebf9d1c7488514c53cb21f8db
Presst0n <pr3sst0n@gmail.com>
2021-04-11 16:31:41 +0200

    Handling form

---
+++ Workshops/Form-handling/js/script.js
@@ -0,0 +1,15 @@
+
+
+let form = document.getElementById('form');
+
+
+const submitForm = (event) => {
+    event.preventDefault();
+
+    let fName = document.querySelector('[name="fname"]');
+    let lName = document.querySelector('[name="lname"]');
+
+    console.log(`Imię: ${fName.value}, Nazwisko: ${lName.value}`);
+}
+
+form.addEventListener('submit', submitForm);
